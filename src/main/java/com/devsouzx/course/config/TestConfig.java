d548e8820c0ab0547db4b1980e4654a9410682da
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-09 20:28:43 -0300

    OrderStatus enum

--- src/main/java/com/devsouzx/course/config/TestConfig.java
+++ src/main/java/com/devsouzx/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.devsouzx.course.config;
 
 import com.devsouzx.course.entities.Order;
 import com.devsouzx.course.entities.User;
+import com.devsouzx.course.entities.enums.OrderStatus;
 import com.devsouzx.course.repositories.OrderRepository;
 import com.devsouzx.course.repositories.UserRepository;
 import org.aspectj.weaver.ast.Or;
@@ -22,16 +23,14 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
-
-
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.CANCELED);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.DELIVERED);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
 
--- src/main/java/com/devsouzx/course/entities/Order.java
+++ src/main/java/com/devsouzx/course/entities/Order.java
@@ -1,7 +1,6 @@
 package com.devsouzx.course.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.devsouzx.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -12,8 +11,6 @@ import java.time.Instant;
 @Table(name = "tb_order")
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Order implements Serializable {
     @Id
@@ -25,4 +22,23 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    private Integer orderStatus;
+
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+        setOrderStatus(orderStatus);
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 }

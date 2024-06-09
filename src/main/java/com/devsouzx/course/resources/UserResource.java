782c204be93b31f2bf8a13357ea039410d6f4b92
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-09 10:00:55 -0300

    Service layer, component registration

--- src/main/java/com/devsouzx/course/resources/UserResource.java
+++ src/main/java/com/devsouzx/course/resources/UserResource.java
@@ -1,18 +1,31 @@
 package com.devsouzx.course.resources;
 
 import com.devsouzx.course.entities.User;
+import com.devsouzx.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    public ResponseEntity<User> findUser() {
-        User u = new User(1L, "joao", "joaoemanuel2215@gmail.com", "99999999", "345456");
-        return ResponseEntity.ok(u);
+    public ResponseEntity<List<User>> findUser() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
     }
 }

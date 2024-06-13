4b81f1837c9dc3df3d2b3ba3280245f56bd82acb
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-13 13:53:50 -0300

    User update

--- src/main/java/com/devsouzx/course/resources/UserResource.java
+++ src/main/java/com/devsouzx/course/resources/UserResource.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
@@ -29,9 +28,23 @@ public class UserResource {
         return ResponseEntity.ok(user);
     }
 
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = userService.update(id, user);
+        return ResponseEntity.ok(user);
     }
 }
